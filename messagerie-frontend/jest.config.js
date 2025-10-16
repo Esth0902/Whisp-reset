24d58c8845010d2cf42046f201a823671bcdc3d8
LaetitiaV <laetitia.voue@gmail.com>
2025-10-16 19:10:27 +0200

    erreur test

--- messagerie-frontend/jest.config.js
+++ messagerie-frontend/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
         '^@clerk/nextjs$': '<rootDir>/__mocks__/@clerk/nextjs.ts',
-        '^next/(.*)$': '<rootDir>/__mocks__/next/$1.ts',
     },
     transform: {
         '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './babel.jest.config.js' }],
